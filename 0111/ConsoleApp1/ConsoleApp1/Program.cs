83c9b17463a82fdaca6b6f4b70573ee02de5974c
Swiew <95688252+Swiew@users.noreply.github.com>
2022-01-11 20:29:18 +0900

    unity 0111
    1월 11일 수업

---
+++ 0111/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                
+        }
+
+        public int TestFunction(int a,int b,int c)
+        {
+            int value = a + b;
+            value = value / c;
+            return value;
+        }
+    }
+}
